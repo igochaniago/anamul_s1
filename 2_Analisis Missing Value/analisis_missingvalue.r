7cdd696cb039397e3f1d4cd56048a6c94bd576c0
Igo Chaniago <65951124+igochaniago@users.noreply.github.com>
2025-08-09 13:53:23 +0700

    Update analisis_missingvalue.r

--- 2_Analisis Missing Value/analisis_missingvalue.r
+++ 2_Analisis Missing Value/analisis_missingvalue.r
@@ -1,13 +1,12 @@
 #Library untuk read dan write data excel
 library(readxl) 
-library(writexl)a
+library(writexl)
 
 #library untuk melakukan imputation
 library(VIM)
 
 #Membaca file data excel "missing data"
-#data <- read_excel("2_Analisis Missing Value/IC_Data Missing.xlsx") 
-data <- read_excel("E:/IGO Programming Project/Github Repo/2_Analisis Missing Value/IC_Data Missing.xlsx")
+data <- read_excel("2_Analisis Missing Value/IC_Data Missing.xlsx") 
 
 #Mengecek file excel sudah dapat terbaca
 View(data)
@@ -36,8 +35,7 @@ colSums(is.na(data))
 
 #=======kNN IMPUTATION==========
 #Membaca file data excel "missing data"
-#data2 <- read_excel("2_Analisis Missing Value/IC_Data Missing.xlsx") 
-data2 <- read_excel("E:/IGO Programming Project/Github Repo/2_Analisis Missing Value/IC_Data Missing.xlsx")
+data2 <- read_excel("2_Analisis Missing Value/IC_Data Missing.xlsx") 
 
 #Mengecek file excel sudah dapat terbaca
 View(data2)
@@ -53,9 +51,9 @@ colSums(is.na(data2))
 hasildata2 <- kNN(data2, k = 9)
 
 #Export data hasil kNN Imputation dari R ke Excel
-write_xlsx(data2, "E:/IGO Programming Project/Github Repo/2_Analisis Missing Value/IC_Data Missing_AfterkNNImp.xlsx")
-#write_xlsx(data, "2_Analisis Missing Value/IC_Data Missing_AfterkNNImp.xlsx") 
+write_xlsx(data, "2_Analisis Missing Value/IC_Data Missing_AfterkNNImp.xlsx")
 
 #Cek apakah masih ada missing data dalam file excel terbaru
 is.na(hasildata2)
+
 colSums(is.na(hasildata2))

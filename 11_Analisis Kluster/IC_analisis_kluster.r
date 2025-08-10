c58767767395606c12892caa6596388065b1d137
Igo Chaniago <65951124+igochaniago@users.noreply.github.com>
2025-08-10 15:19:31 +0700

    Update IC_analisis_kluster.r

--- 11_Analisis Kluster/IC_analisis_kluster.r
+++ 11_Analisis Kluster/IC_analisis_kluster.r
@@ -6,8 +6,7 @@ library(cluster)
 library(factoextra)
 
 #Membaca file data excel
-data <- read_excel("E:/IGO Programming Project/Github Repo/11_Analisis Kluster/IC_Data Analisis Cluster.xlsx")
-#data <- read_excel("11_Analisis Kluster/IC_Data Analisis Cluster.xlsx")
+data <- read_excel("11_Analisis Kluster/IC_Data Analisis Cluster.xlsx")
 
 #Mengecek file excel sudah dapat terbaca
 View(data)
@@ -24,4 +23,5 @@ fviz_nbclust(normdata, kmeans, method = "silhouette") #metode silhouette
 #K-means dengan cluster = 2
 final2 <- kmeans(normdata,2,nstart=25)
 final2
+
 fviz_cluster(final2, data = normdata)
